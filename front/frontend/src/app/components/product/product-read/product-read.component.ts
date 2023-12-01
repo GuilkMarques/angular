943e3abcc82b059d11bd741493d73046b06ea01e
Guilherme Marques <guilhermeluciano99@gmail.com>
2023-12-01 10:15:57 -0300

    Add files via upload

---
+++ front/frontend/src/app/components/product/product-read/product-read.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-read',
+  templateUrl: './product-read.component.html',
+  styleUrl: './product-read.component.css'
+})
+export class ProductReadComponent {
+
+}
